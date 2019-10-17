a0800344fcbcc29a4c3c4a619f4b6c18af8cc4ba
Evelina <evelinawahlstrom18@gmail.com>
2019-10-17 17:59:45 +0200

    set-up-react-redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import store from './store'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router-dom'
+import Home from './components/Home'
 
-function App() {
+class App extends Component {
+  componentDidMount() {
+    // query parameters --> add after /teams?q=blabla,
+    // add this using httpie (?)
+    fetch("http://localhost:4000/team")
+  } 
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store={store}>
+    <div>
+    <Route path="/" exact component={Home} />
     </div>
-  );
+    </Provider>
+  )
+}
 }
 
-export default App;
+export default App
